3898114fb069fed21dea67faac3962086b5f52f2
danneg <dana@boxalino.com>
2020-08-19 20:03:03 +0200

    [autocomplete] update on template strategy

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,10 +1,15 @@
 var config = {
     map: {
         '*': {
-            nouislider: 'Boxalino_RealTimeUserExperienceIntegration/js/nouislider',
+            nouislider: 'Boxalino_RealTimeUserExperienceIntegration/js/nouislider'
         }
     },
-    path: {
-        'nouislider': 'Boxalino_RealTimeUserExperienceIntegration/js/nouislider',
+    deps: ["rtuxApiHelper"],
+    config: {
+        mixins: {
+            'Magento_Search/js/form-mini': {
+                'Boxalino_RealTimeUserExperienceIntegration/js/rtuxApiAutocompleteMixin': true
+            }
+        }
     }
 };
