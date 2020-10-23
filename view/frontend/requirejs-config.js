e4a060ca09bedd0028473a7d4aa6fb5cc200fa94
danneg <dana@boxalino.com>
2020-10-23 19:26:28 +0200

    [default init setup]

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,16 +1,8 @@
 var config = {
     map: {
         '*': {
-            nouislider: 'BoxalinoClientProject_BoxalinoIntegration/js/nouislider',
             rtuxApiTracker: 'BoxalinoClientProject_BoxalinoIntegration/js/rtuxApiTracker'
         }
     },
-    deps: ["rtuxApiHelper"],
-    config: {
-        mixins: {
-            'Magento_Search/js/form-mini': {
-                'BoxalinoClientProject_BoxalinoIntegration/js/rtuxApiAutocompleteMixin': true
-            }
-        }
-    }
+    deps: ["rtuxApiHelper"]
 };
