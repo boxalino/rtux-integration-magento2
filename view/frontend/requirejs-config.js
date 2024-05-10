1776b00c13c8abcc4d8de5f84da055e6495208e8
Dana Negrescu <dana@boxalino.com>
2024-05-10 19:12:42 +0200

    [search&navigation]

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -2,15 +2,6 @@ var config = {
     map: {
         '*': {
             nouislider: 'BoxalinoClientProject_BoxalinoIntegration/js/nouislider',
-            rtuxApiTracker: 'BoxalinoClientProject_BoxalinoIntegration/js/rtuxApiTracker'
-        }
-    },
-    deps: ["rtuxApiHelper"],
-    config: {
-        mixins: {
-            'Magento_Search/js/form-mini': {
-                'BoxalinoClientProject_BoxalinoIntegration/js/rtuxApiAutocompleteMixin': true
-            }
         }
     }
 };
