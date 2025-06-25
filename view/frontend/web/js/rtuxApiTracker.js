ac5f3608e8efa4bdba697ab88054c7a42f7e1b25
danneg <dana.negrescu@boxalino.com>
2025-06-25 12:25:52 +0200

    [tracking] tracking update to include cookie restrictions

--- view/frontend/web/js/rtuxApiTracker.js
+++ view/frontend/web/js/rtuxApiTracker.js
@@ -15,7 +15,6 @@ define([
     'Boxalino_RealTimeUserExperience/js/api/events/purchase',
 ], function (
     $,
-    ViewPageEvent,
     ViewNavigationEvent,
     ViewSearchEvent,
     ViewProductEvent,
@@ -33,14 +32,33 @@ define([
          * @private
          */
         _create: function () {
-            if(!$.boxalino.rtuxApiHelper.hasCookieRestriction()) {
-                $.boxalino.rtuxApiHelper.useDebugCookie();
+            if(this.isAllowed()) {
+                // uncomment this if the the HTML tracking attributes must be tested
+                // do not do it in production environment
+                // $.boxalino.rtuxApiHelper.useDebugCookie();
 
                 this.events = [];
-
                 this.registerDefaultEvents();
                 this.handleEvents();
             }
+        },
+
+        isAllowed: function() {
+            if($.boxalino.rtuxApiHelper.hasCookieRestriction()) {
+                if($.boxalino.rtuxApiHelper.isUserNotAllowed()) {
+                    if($.boxalino.rtuxApiHelper.userCookieConfirmed()) {
+                        return true;
+                    }
+                    $.boxalino.rtuxApiHelper.getApiProfileId();
+                    $(document).on('user:allowed:save:cookie', function(event) {
+                        $.boxalino.rtuxApiHelper.setUserCookieConsent(true);
+                        this._create();
+                    }.bind(this));
+                    return false;
+                }
+            }
+
+            return true;
         },
 
         /**
@@ -59,7 +77,6 @@ define([
          * @public
          */
         registerDefaultEvents() {
-            this.registerEvent(ViewPageEvent);
             this.registerEvent(ViewProductEvent);
             this.registerEvent(ViewSearchEvent);
             this.registerEvent(ViewNavigationEvent);
